e16ef96253f2da44d9b8d54c3f075a7a5d3b90cb
Noam Norman <109430971+noamnorman21@users.noreply.github.com>
2024-01-21 10:18:29 +0200

    Add files via upload
    DotNetCore Server Side - Server/DotNetCore/WebApi

---
+++ Server/DotNetCore/WebAPI/Controllers/OrderDetailsController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Newtonsoft.Json;
+using System.Data;
+using System.Linq;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderDetailsController : ControllerBase
+    {
+        private IConfiguration _configuration;
+
+        public OrderDetailsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        [Route("AddOrder")]
+        public async Task<JsonResult> AddOrder([FromBody] OrderViewModel newOrder)
+        {
+            try
+            {
+                string query = "INSERT INTO OrderDetails (FirstName, LastName, Address, Email, OrderItems) VALUES (@FirstName, @LastName, @Address, @Email, @OrderItems)";
+                using (SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("ShoppingAppCon")))
+                {
+                    await connection.OpenAsync();
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@FirstName", newOrder.FirstName);
+                        command.Parameters.AddWithValue("@LastName", newOrder.LastName);
+                        command.Parameters.AddWithValue("@Address", newOrder.Address);
+                        command.Parameters.AddWithValue("@Email", newOrder.Email);
+                        command.Parameters.AddWithValue("@OrderItems", JsonConvert.SerializeObject(newOrder.OrderItems));
+
+                        await command.ExecuteNonQueryAsync();
+                    }
+                }
+
+                return new JsonResult("Added Successfully");
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult($"Error: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Server/DotNetCore/WebAPI/Models/OrderViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace WebAPI.Models
+{
+    public class OrderViewModel
+    {
+        public int OrderId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Address { get; set; }
+        public string? Email { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public List<OrderItemViewModel>? OrderItems { get; set; }
+    }
+
+    public class OrderItemViewModel
+    {
+        public string? Name { get; set; }
+        public int? Quantity { get; set; }
+    }
+}
